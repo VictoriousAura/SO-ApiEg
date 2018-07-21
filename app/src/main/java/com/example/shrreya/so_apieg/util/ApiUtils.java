f4aa6519d9c45607682e6bb1858da4c13eb94839
VictoriousAura <shrreya.behll@hotmail.com>
2018-07-21 16:57:10 +0530

    initial commit

---
+++ app/src/main/java/com/example/shrreya/so_apieg/util/ApiUtils.java
@@ -0,0 +1,12 @@
+package com.example.shrreya.so_apieg.util;
+
+import com.example.shrreya.so_apieg.data.remote.RetrofitClient;
+import com.example.shrreya.so_apieg.data.remote.SOService;
+
+public class ApiUtils {
+    public static final String BASE_URL = "https://api.stackexchange.com/2.2/";
+
+    public static SOService getSOService() {
+        return RetrofitClient.getClient(BASE_URL).create(SOService.class);
+    }
+}
